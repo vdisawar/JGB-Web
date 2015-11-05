2ed3fdcf2af88fcd25b8db71f1708f1f011c2a92
Vishal Disawar <disawar2@illinois.edu>
2015-11-05 15:08:16 -0600

    first commit

---
+++ assets/js/service.js
@@ -0,0 +1,63 @@
+var webApp = angular.module('FacebookProvider', []);
+webApp.factory('Facebook', function ($rootScope) {
+    return {
+        // Calls the FB login status function and broadcasts results
+        getLoginStatus:function () {
+            FB.getLoginStatus(function (response) {
+                $rootScope.$broadcast("fb_statusChange", {'status':response.status});
+            }, true);
+        },
+        // Performs login and considers cases for:
+        // Connected - broadcast connected
+        // Not Auth - broadcast not authorized
+        // Unknown - Perform error checking
+        login:function () {
+            FB.getLoginStatus(function (response) {
+                switch (response.status) {
+                    case 'connected':
+                        $rootScope.$broadcast('fb_connected', {facebook_id:response.authResponse.userID});
+                        break;
+                    case 'not_authorized': console.log("Not Authorized");
+                    case 'unknown':
+                        FB.login(function (response) {
+                            if (response.authResponse) {
+                                $rootScope.$broadcast('fb_connected', {
+                                    facebook_id:response.authResponse.userID,
+                                    userNotAuthorized:true
+                                });
+                            } else {
+                                $rootScope.$broadcast('fb_login_failed');
+                            }
+                        }, {scope:'read_stream, publish_stream, email'});
+                        break;
+                    default:
+                        FB.login(function (response) {
+                            if (response.authResponse) {
+                                $rootScope.$broadcast('fb_connected', {facebook_id:response.authResponse.userID});
+                                $rootScope.$broadcast('fb_get_login_status');
+                            } else {
+                                $rootScope.$broadcast('fb_login_failed');
+                            }
+                        });
+                        break;
+                }
+            }, true);
+        },
+        // Facebook logout
+        logout:function () {
+            FB.logout(function (response) {
+                if (response) {
+                    $rootScope.$broadcast('fb_logout_succeded');
+                } else {
+                    $rootScope.$broadcast('fb_logout_failed');
+                }
+            });
+        },
+        // Facebook unsubscribe from the app
+        unsubscribe:function () {
+            FB.api("/me/permissions", "DELETE", function (response) {
+                $rootScope.$broadcast('fb_get_login_status');
+            });
+        }
+    };
+});
